a5e5f124913d6f8de1def18346a59212a6441d2a
Jose Antonio Mouriño Chas <mouri32@gmail.com>
2021-05-18 15:40:00 +0200

    books-routing commented imports and routes path that not in use

--- src/app/main/books/books-routing.module.ts
+++ src/app/main/books/books-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BooksHomeComponent } from "./books-home/books-home.component";
-import { BooksDetailComponent } from './books-detail/books-detail.component';
-import { BooksEditComponent } from './books-edit/books-edit.component';
-import { BooksAddComponent } from './books-add/books-add.component';
+//import { BooksDetailComponent } from './books-detail/books-detail.component';
+//import { BooksEditComponent } from './books-edit/books-edit.component';
+//import { BooksAddComponent } from './books-add/books-add.component';
 
 
 const routes: Routes = [
-{ path: '', component: BooksHomeComponent },
-{ path: 'add', component: BooksAddComponent },
-{ path: ':bookid', component: BooksDetailComponent },
-{ path: ':bookid/edit', component: BooksEditComponent }
+{ path: '', component: BooksHomeComponent }//,
+//{ path: 'add', component: BooksAddComponent },
+//{ path: ':bookid', component: BooksDetailComponent },
+//{ path: ':bookid/edit', component: BooksEditComponent }
 ];
 
 @NgModule({
