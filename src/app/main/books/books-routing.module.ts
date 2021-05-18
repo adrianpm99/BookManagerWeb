e3dbd2a4dd82eaf942fc69bca02d1d54c4cb8e12
barrio <you@example.com>
2021-05-18 15:16:52 +0200

    Added bookDetails component

--- src/app/main/books/books-routing.module.ts
+++ src/app/main/books/books-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BooksHomeComponent } from "./books-home/books-home.component";
-
+import { BooksDetailsComponent } from './books-details/books-details.component';
 
 const routes: Routes = [{
   path : '',
   component: BooksHomeComponent
+},
+{
+  path: ":BOOKID",
+  component: BooksDetailsComponent
 }];
 
 @NgModule({
