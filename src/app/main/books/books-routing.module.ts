10f8c80c0cedf9d745efbd39487a76158cb5b858
Jose Antonio Mouriño Chas <mouri32@gmail.com>
2021-05-18 15:15:02 +0200

    books-routing add, edit, detail

--- src/app/main/books/books-routing.module.ts
+++ src/app/main/books/books-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BooksHomeComponent } from "./books-home/books-home.component";
+import { BookDetailComponent } from './detail/books-detail.component';
+import { BookEditComponent } from './edit/books-edit.component';
+import { BookAddComponent } from './add/books-add.component';
 
 
-const routes: Routes = [{
-  path : '',
-  component: BooksHomeComponent
-}];
+const routes: Routes = [
+{ path: '', component: BooksHomeComponent },
+{ path: 'add', component: BooksAddComponent },
+{ path: ':bookid', component: BooksDetailComponent },
+{ path: ':bookid/edit', component: BooksEditComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
