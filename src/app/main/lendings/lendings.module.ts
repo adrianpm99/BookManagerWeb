855b08a8cfd83c7f10f8a1155b68b97833586b9c
Adrian Pereiro <adrian.pereiro@campusdual.com>
2021-05-19 11:16:59 +0200

    lending detail

--- src/app/main/lendings/lendings.module.ts
+++ src/app/main/lendings/lendings.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OntimizeWebModule } from "ontimize-web-ngx";
+
 import { LendingsRoutingModule } from './lendings-routing.module';
 import { LendingsHomeComponent } from "./lendings-home/lendings-home.component";
 import { LendingsDetailComponent } from './lendings-detail/lendings-detail.component';
-
 
 @NgModule({
   declarations: [LendingsDetailComponent,
