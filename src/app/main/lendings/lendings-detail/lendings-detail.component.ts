2477004abb5c1e5fe826c0fbddcd485b803c9a8b
Adrian Pereiro <adrian.pereiro@campusdual.com>
2021-05-20 16:18:06 +0200

    lending detail added

--- src/app/main/lendings/lendings-detail/lendings-detail.component.ts
+++ src/app/main/lendings/lendings-detail/lendings-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SystemJsNgModuleLoader, ViewChild } from '@angular/core';
+import { OFormComponent, OQueryDataArgs, OTableComponent } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-lendings-detail',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LendingsDetailComponent implements OnInit {
 
+  @ViewChild("testForm", {static: true}) testForm: OFormComponent;
+
   constructor() { }
 
+  //@ViewChild('lendinDetail',{ static: false }) lendingDetail : OTableComponent;
+
   ngOnInit() {
+    //let selected = this.lendingDetail.getSelectedItems();
+    //let accountId = selected[0][0];
+    //console.log(accountId);
+    console.log(this.testForm);
+    document.addEventListener("ready", carga, false);
+    function carga(){
+      //var adios = self.FormData;
+      //console.log(adios);
+      console.log(0);
+
+    }
   }
 
 }
