2c0f181d65892e93b13753a9ae4db56f690b016f
Jose Antonio Mouriño Chas <mouri32@gmail.com>
2021-05-26 15:16:56 +0200

    dev/editbooks

--- src/app/main/books/books-routing.module.ts
+++ src/app/main/books/books-routing.module.ts
@@ -4,13 +4,15 @@ import { BooksHomeComponent } from "./books-home/books-home.component";
 import { BooksDetailsComponent } from './books-details/books-details.component';
 import { BooksAddComponent } from './books-add/books-add.component';
 import { CopiesAddComponent} from '../copies/copies-add/copies-add.component';
+import { BooksEditComponent } from './books-edit/books-edit.component';
 
 
 const routes: Routes = [
 { path: '', component: BooksHomeComponent },   // ... /books
 { path: 'new', component: BooksAddComponent }, // ... /books/new
 { path: ':BOOKID', component: BooksDetailsComponent }, // ... /books/3
-{ path: ':BOOKID/copies/new', component: CopiesAddComponent } // ... /books/3/copies/new
+{ path: ':BOOKID/copies/new', component: CopiesAddComponent }, // ... /books/3/copies/new
+{ path: ':BOOKID/edit', component:BooksEditComponent}  //.../books/edit
 ];
 
 @NgModule({
