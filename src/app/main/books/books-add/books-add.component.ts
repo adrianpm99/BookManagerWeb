a6ee638f9b326d38db07db7509464a99ab2ae862
barrio <you@example.com>
2021-05-26 17:06:16 +0200

    Changes on books inputs and redesing i18n

--- src/app/main/books/books-add/books-add.component.ts
+++ src/app/main/books/books-add/books-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidationErrors, FormControl, ControlContainer, NgControlStatus, RequiredValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-books-add',
@@ -11,5 +12,49 @@ export class BooksAddComponent implements OnInit {
 
   ngOnInit() {
   }
+  /*//Pattern (All)
+  validDefault (control: FormControl): ValidationErrors {
 
+    let result;
+    console.log(control);
+
+    if(control.value && control){
+      result = {'defaultError': true};
+    }else{
+      result = {};
+    }
+    return result;
+
+  }//isValid()*/
+
+  //Pattern ISBN
+  validISBN(control: FormControl): ValidationErrors {
+
+    let regexp = new RegExp("^[\\d]{13}$");
+    let result = {};
+
+    if (!regexp.test(control.value.toString())) {
+      result = {'ISBNFormat': true};
+    }
+    return result;
+
+  }//validISBN()
+
+  //Pattern BookPublicationYear
+  validBookPublicationYear(control: FormControl): ValidationErrors {
+
+    var fecha = new Date();
+    let regexp = new RegExp("^[\\d]{4}$");
+    let result = {};
+
+    if (control.value){
+      if (!regexp.test(control.value.toString())) {
+        result = {'bookPublicationYearFormat': true};
+      }else if(control.value > fecha.getFullYear()){
+        result = {'bookPublicationYearFormatToday': true};
+      }
+    }
+    return result;
+  /*pattern="^(?!0)\\d{0,3}$"*/
+  }//validBookPublicationYear()
 }
