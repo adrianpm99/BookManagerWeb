5931b659d05a20fac447733e2d1d613ae3c815a0
Miguel <miguel.gonzalez@campusdual.com>
2021-06-01 12:06:11 +0200

    lending details on home

--- src/app/main/home/home-routing.module.ts
+++ src/app/main/home/home-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LendingsDetailComponent } from '../lendings/lendings-detail/lendings-detail.component';
 import { HomeComponent } from './home.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: ':LENDINGID', component: LendingsDetailComponent }
 
 ];
 
