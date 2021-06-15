6ee5b1ff21e7e5feac0de82f9d03dfd74ef0fc48
laura <laura.gtaillefert@campusdual.com>
2021-06-15 15:50:19 +0200

    customer-customer-edit

--- src/app/main/customers-customers/customers-customers-routing.module.ts
+++ src/app/main/customers-customers/customers-customers-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CustomersCustomersHomeComponent } from './customers-customers-home/customers-customers-home.component';
 import { CustomersDetailComponent } from '../customers/customers-detail/customers-detail.component';
-
+import { CustomersEditComponent } from '../customers/customers-edit/customers-edit.component';
 
 const routes: Routes = [
   {path : '', component: CustomersCustomersHomeComponent},
-  {path: ':CUSTOMERID', component: CustomersDetailComponent },];
+  {path: ':CUSTOMERID', component: CustomersDetailComponent },
+  {path: ':CUSTOMERID/edit', component: CustomersEditComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/main/customers-customers/customers-customers.module.ts
+++ src/app/main/customers-customers/customers-customers.module.ts
@@ -4,11 +4,13 @@ import { OntimizeWebModule } from "ontimize-web-ngx";
 import { CustomersCustomersRoutingModule } from './customers-customers-routing.module';
 import { CustomersCustomersHomeComponent } from './customers-customers-home/customers-customers-home.component';
 import { CustomersDetailComponent } from '../customers/customers-detail/customers-detail.component';
+import { CustomersEditComponent } from '../customers/customers-edit/customers-edit.component';
 
 
 @NgModule({
   declarations: [CustomersCustomersHomeComponent,
-    CustomersDetailComponent],
+    CustomersDetailComponent,
+    CustomersEditComponent],
   imports: [
     CommonModule,
     OntimizeWebModule,
