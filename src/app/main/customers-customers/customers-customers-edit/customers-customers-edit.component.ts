8fa03f9252ed95ed528257b42c72aea0742b06a5
Jose Antonio Mouriño Chas <mouri32@gmail.com>
2021-06-16 18:52:45 +0200

    dev/addEditFielPasswordInCustomers-customers-edit-component

--- src/app/main/customers-customers/customers-customers-edit/customers-customers-edit.component.ts
+++ src/app/main/customers-customers/customers-customers-edit/customers-customers-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-customers-customers-edit',
@@ -11,5 +12,20 @@ export class CustomersCustomersEditComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  //Pattern Phone, checks if the provided number is exactly 9 characters
+  validCustomerPhone(control: FormControl): ValidationErrors {
+
+    let regexp = new RegExp("^[\\d]{9}$");
+    let result = {};
+
+    if (!regexp.test(control.value)) {
+      result = {'CustomerPhoneFormat': true};
+    }
+    return result;
+
+  }//validCustomerFormat()
+
+
 
 }
